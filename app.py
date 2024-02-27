968cea24a37016c69c91317d65a1843434425176
Nathan <nathan.salbego@gmail.com>
2024-02-27 16:05:45 +0100

    Initialisation database

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ from database import *
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database/database.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
 db.init_app(app)
 with app.test_request_context():
  init_database()
