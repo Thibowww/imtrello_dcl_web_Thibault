431aaaa1c893cb0828c17ae60fee292675e1c066
Ilian BEN AMAR <ilian20000@hotmail.fr>
2024-03-05 11:09:49 +0100

    update gitignore to ignore db

--- database/database.py
+++ database/database.py
@@ -1,14 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
-
 db = SQLAlchemy()
 
 
 def init_database():
     db.create_all()
 
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    label = db.Column(db.Text)
-    isDone = db.Column(db.Boolean)
 
--- database/models.py
+++ database/models.py
@@ -1,3 +1,8 @@
 from database.database import db
 
 
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.Text)
+    isDone = db.Column(db.Boolean)
+
