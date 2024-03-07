2a824b0450d6da93414267d03a59c0eb0e6efa46
Nathan <nathan.salbego@gmail.com>
2024-03-07 08:26:07 +0100

    Fix import database.db

--- database/database.py
+++ database/database.py
@@ -1,4 +1,6 @@
-from . import db  # Assuming that the db instance is defined in the same directory
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def init_database():
     db.create_all()
